781a8c6ff03a68eb2641af38efa603ce1067db78
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-09-24 13:07:42 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Player(iframe);
+
+function saveTimeToLocalStorage() {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}
+
+function setTimeFromLocalStorage() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+}
+
+player.on('timeupdate', throttle(saveTimeToLocalStorage, 1000));
+
+player.ready().then(() => {
+  setTimeFromLocalStorage();
+  player.play();
+});
