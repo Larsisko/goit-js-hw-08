69961f79fd3fff65dc356d44ad89d6cde3148a8d
Larsisko <arkadiusz.miazga1988@gmail.com>
2023-09-24 12:16:09 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryImageList = galleryItems
+  .map(image => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" title="${image.description}" data-source="${image.original}" />
+</a>
+    </li>`;
+  })
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryImageList);
+const lightbox = new SimpleLightbox('.gallery a', {
+  //   captionPosition: 'outside',
+  captionDelay: 250,
+  nav: false,
+  close: false,
+  showCounter: false,
+  captions: true,
+
+  captionData: 'alt',
+  disableScroll: true,
+  scrollZoom: false,
+});
